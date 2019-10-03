6d4f964e6a5578cfeb3c739acd83899bfbd25a21
SKhandorin <hsatig@mail.ru>
2019-10-03 10:13:05 +0500

    Initial commit

---
+++ src/skhandorin/Main.java
@@ -0,0 +1,8 @@
+package skhandorin;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/skhandorin/TestSimpleTimeClient.java
@@ -0,0 +1,14 @@
+package skhandorin;
+
+import java.time.*;
+import java.lang.*;
+import java.util.*;
+
+public class TestSimpleTimeClient {
+    public static void main(String... args) {
+        TimeClient myTimeClient = new SimpleTimeClient();
+        System.out.println("Current time: " + myTimeClient.toString());
+        System.out.println("Time in California: " +
+                myTimeClient.getZonedDateTime("UTC+3").toString());
+    }
+}
